73362ec1ddffc72a1712e97ca130c595c5d8ccf8
Zachary Belford <belfordz66@gmail.com>
2019-01-24 20:40:27 -0800

    fix: cleanup and make more real

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
+const { reduce } = require('lodash');
 const fs = require('fs');
 const refParser = require('json-schema-ref-parser');
 const jsf = require('json-schema-faker');
 const jayson = require('jayson');
 
-const schemaFile = fs.readFileSync('./openrpc.json', 'utf8');
+let schemaFile;
+try {
+  schemaFile = fs.readFileSync(`${process.cwd()}/openrpc.json`, 'utf8');
+} catch (e) {
+  console.error(new Error(`Could not find openrpc.json file in ${process.cwd()}`));
+  process.exit(1);
+}
+
 const rawSchema = JSON.parse(schemaFile);
 
 refParser.dereference(rawSchema).then((schema) => {
-  const server = jayson.server(schema.methods.reduce((result, methodObject, methodName) => {
+  const server = jayson.server(reduce(schema.methods, (result, methodObject, methodName) => {
     result[methodName] = (args, cb) => {
       return cb(null, generateResponse(schema, methodName));
     };

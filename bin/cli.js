9bbf875c8e03e96189dd47ffa9e50bb09431f610
Zachary Belford <belfordz66@gmail.com>
2019-02-06 18:41:07 -0800

    fix: big update

---
+++ bin/cli.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+const program = require('commander');
+const server = require('../src/server');
+
+program
+  .version(require('./get-version'))
+  .option('-s, --schema [schema]', 'JSON string or a Path/Url pointing to an open rpc schema')
+  .action(() => {
+    server('http', 3000, program.schema)
+      .catch((e) => {
+        console.error(e);
+        process.exit(1);
+      });
+  })
+  .parse(process.argv);
---
+++ src/server.js
@@ -0,0 +1,14 @@
+const { reduce } = require('lodash');
+const fs = require('fs');
+const jayson = require('jayson');
+const parseSchema = require('@open-rpc/schema-utils-js');
+const buildMethodHandlerMapping = require('./generate-method-mapping');
+
+const server = async (protocol, port, schemaLocation) => {
+  const schema = await parseSchema(schemaLocation);
+  const server = jayson.server(buildMethodHandlerMapping(schema));
+  server[protocol]().listen(port);
+  console.log(`service is listening on port ${port} via the ${protocol} protocol`);
+};
+
+module.exports = server;

3c6cae3fe99df0984b4a6fe22d30da987de8de18
Shane <jonas.shane@gmail.com>
2019-02-11 13:28:16 -0800

    fix: typo
    Co-Authored-By: BelfordZ <belfordz66@gmail.com>

--- bin/cli.js
+++ bin/cli.js
@@ -4,7 +4,7 @@ const server = require('../src/server');
 
 program
   .version(require('./get-version'))
-  .option('-s, --schema [schema]', 'JSON string or a Path/Url pointing to an open rpc schema')
+  .option('-s, --schema [schema]', 'JSON string or a Path/Url pointing to an OpenRPC Schema')
   .action(() => {
     server('http', 3000, program.schema)
       .catch((e) => {

975f42429ad306b9c56ec8275fd44d1fd66cab2a
Zachary Belford <belfordz66@gmail.com>
2020-07-29 23:44:34 -0700

    fix: improve testing and coverage

--- src/cli.test.ts
+++ src/cli.test.ts
@@ -1,29 +1,27 @@
 import { exec } from "child_process";
 import examples from "@open-rpc/examples";
 
-import * as http from "http";
-
-const data = JSON.stringify({
-  id: 1,
-  jsonrpc: "2.0",
-  method: "addition",
-  params: [2, 2],
-});
-
-const options = {
-  hostname: "0.0.0.0",
-  port: 3333,
-  path: "/",
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-    "Content-Length": data.length,
-  },
-};
+import http from "http";
 
 describe("cli", () => {
   it("can be run with an example", (done) => {
-    expect.assertions(2);
+    const data = JSON.stringify({
+      id: 1,
+      jsonrpc: "2.0",
+      method: "addition",
+      params: [2, 2],
+    });
+
+    const options = {
+      hostname: "0.0.0.0",
+      port: 3333,
+      path: "/",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Content-Length": data.length,
+      },
+    };
     const childProc = exec(`node ./build/cli.js -d '${JSON.stringify(examples.simpleMath)}'`);
 
     setTimeout(() => {
@@ -31,8 +29,8 @@ describe("cli", () => {
         expect(res.statusCode).toBe(200);
 
         res.on("data", (d: any) => {
+          expect(JSON.parse(d.toString()).result).toBe(4);
           childProc.kill("SIGHUP");
-          expect(JSON.parse(d).result).toBe(4);
           done();
         });
       });
@@ -41,5 +39,72 @@ describe("cli", () => {
       req.write(data);
       req.end();
     }, 1000);
+  });
+
+  it("can run in service mode", (done) => {
+    const childProc = exec(`node ./build/cli.js -m service`);
+
+    const requestBody = JSON.stringify({
+      id: 1,
+      jsonrpc: "2.0",
+      method: "mock",
+      params: [examples.simpleMath],
+    });
+
+    const reqObj = {
+      hostname: "0.0.0.0",
+      port: 3333,
+      path: "/",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Content-Length": requestBody.length,
+      },
+    };
+
+    setTimeout(() => {
+      const req = http.request(reqObj, (res: any) => {
+        expect(res.statusCode).toBe(200);
+
+        res.on("data", (d: any) => {
+          expect(JSON.parse(d.toString()).result).toBe("simpleMath-1.0.0");
+
+          const simpleMathReqBody = JSON.stringify({
+            id: 1,
+            jsonrpc: "2.0",
+            method: "addition",
+            params: [2, 2],
+          });
+
+          const simpleMathReqObj = {
+            hostname: "0.0.0.0",
+            port: 3333,
+            path: "/simpleMath-1.0.0",
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              "Content-Length": simpleMathReqBody.length,
+            },
+          };
+          const insideReq = http.request(simpleMathReqObj, (insideRes: any) => {
+            expect(insideRes.statusCode).toBe(200);
+
+            insideRes.on("data", (insideD: any) => {
+              expect(JSON.parse(insideD.toString()).result).toBe(4);
+              childProc.kill("SIGHUP");
+              done();
+            });
+          });
+
+          insideReq.on("error", (error: any) => { throw error; });
+          insideReq.write(simpleMathReqBody);
+          insideReq.end();
+        });
+      });
+
+      req.on("error", (error: any) => { throw error; });
+      req.write(requestBody);
+      req.end();
+    }, 4000);
   });
 });
